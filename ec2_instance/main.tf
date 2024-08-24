ebd1efe89b4599c51d46780af73c89681630b862
alexplayer15 <alexplayer15@icloud.com>
2024-08-24 18:50:47 +0100

    now test works uncomment tf destroy

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terratest_ec2" {
     ami = "ami-0b25f6ba2f4419235"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     subnet_id = var.terratest_subnet_id
     vpc_security_group_ids = [var.allow_all_sg_id]
 
